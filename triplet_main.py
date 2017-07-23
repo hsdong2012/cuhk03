9e610242b30e40ae4af81baabed3a2fbe8558120
cuhk-hbsun <hk95635228@163.com>
2017-07-23 17:56:17 +0800

    get corrext anchor, positive, negative dataset

--- triplet_main.py
+++ triplet_main.py
@@ -23,15 +23,15 @@ from utils import Logger, AverageMeter, accuracy, mkdir_p, savefig
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch CUHK03 Example')
-parser.add_argument('--train-batch-size', type=int, default=24, metavar='N',
-                    help='input batch size for training (default: 24)')
+parser.add_argument('--train-batch-size', type=int, default=20, metavar='N',
+                    help='input batch size for training (default: 20)')
 parser.add_argument('--test-batch-size', type=int, default=10, metavar='N',
                     help='input batch size for testing (default: 10)')
 parser.add_argument('--epochs', type=int, default=60, metavar='N',
                     help='number of epochs to train (default: 60)')
 # lr=0.1 for resnet, 0.01 for alexnet and vgg
-parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                    help='learning rate (default: 0.01)')
+parser.add_argument('--lr', type=float, default=0.005, metavar='LR',
+                    help='learning rate (default: 0.005)')
 parser.add_argument('--momentum', type=float, default=0.005, metavar='M',
                     help='SGD momentum (default: 0.5)')
 parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
@@ -56,12 +56,12 @@ if args.cuda:
 # get triplet dataset
 def _get_triplet_data(train):
     with h5py.File('cuhk-03.h5','r') as ff:
-        class_num = 800
+        class_num = 1000
         a = np.array([ff['a']['train'][str(i)][0] for i in range(class_num)])
         a_label = np.array([i for i in range(class_num)])
         b = np.array([ff['b'][train][str(i)][0] for i in range(class_num)])
         b_label = np.array([i for i in range(class_num)])
-        c = np.array([ff['a'][train][str(i)][0] for i in reversed(range(class_num))])
+        c = np.array([ff['b'][train][str(i)][0] for i in reversed(range(class_num))])
         c_label = np.array([i for i in reversed(range(class_num))])
         a_trans = a.transpose(0, 3, 1, 2)
         b_trans = b.transpose(0, 3, 1, 2)
@@ -78,14 +78,22 @@ def _get_triplet_data(train):
 
         dataset = np.array(dataset)
         label = np.array(label)
-        triplet_dataset = torch.from_numpy(dataset)
-        triplet_label = torch.from_numpy(label)
+        tensor_dataset = torch.from_numpy(dataset)
+        tensor_label = torch.from_numpy(label)
 
         transform = transforms.Normalize(mean=[0.367, 0.362, 0.357], std=[0.244, 0.247, 0.249])
         for j in range(class_num*3):
-            triplet_dataset[j] = transform(triplet_dataset[j])
+            tensor_dataset[j] = transform(tensor_dataset[j])
+	
+	# print(tensor_dataset.size())
+	
+	triplet_dataset = tensor_dataset.resize_(class_num, 3, tensor_dataset.size(1), tensor_dataset.size(2), tensor_dataset.size(3))
 
-        return triplet_dataset, triplet_label, class_num
+	# print('triplet_dataset size: ', triplet_dataset.size())	
+
+	triplet_label = tensor_label.resize_(class_num, 3) 
+
+        return (triplet_dataset, triplet_label, class_num)
 
 
 # get validation dataset of five camera pairs
@@ -109,7 +117,7 @@ def _get_data(val_or_test):
         for j in range(num2):
             camere2[j] = transform(camere2[j])
 
-        return camere1, camere2
+        return (camere1, camere2)
 
 
 
@@ -118,17 +126,13 @@ def train_model(train_loader, model, criterion, optimizer, epoch):
     model.train()
 
     for batch_idx, (inputs, targets) in enumerate(train_loader):
-        pair_num = args.train_batch_size / 3
-        # print(inputs.size())
-        # print(pair_num)
-        anchor = torch.FloatTensor(pair_num, inputs.size(1), inputs.size(2), inputs.size(3)).zero_()
-        positive = torch.FloatTensor(pair_num, inputs.size(1), inputs.size(2), inputs.size(3)).zero_()
-        negative = torch.FloatTensor(pair_num, inputs.size(1), inputs.size(2), inputs.size(3)).zero_()
-        for i in range(pair_num):
-            anchor[i] = inputs[3*i]
-            positive[i] = inputs[3*i+1]
-            negative[i] = inputs[3*i+2]
+ 
 
+	triplet_pair = torch.split(inputs, 1, 1)
+	anchor = triplet_pair[0].resize_(args.train_batch_size, inputs.size(2), inputs.size(3), inputs.size(4))
+	positive = triplet_pair[1].resize_(args.train_batch_size, inputs.size(2), inputs.size(3), inputs.size(4))
+	negative = triplet_pair[2].resize_(args.train_batch_size, inputs.size(2), inputs.size(3), inputs.size(4))
+	
         anchor = anchor.float()  # with size of (batch_size/3 * 3 * 224 * 224)
         positive = positive.float()
         negative = negative.float()
@@ -138,6 +142,8 @@ def train_model(train_loader, model, criterion, optimizer, epoch):
         optimizer.zero_grad()
         # compute output
         outputs1 = model(anchor)
+	# print('anchor: ', anchor.size())
+	# print('output11: ', outputs1.size())
         outputs2 = model(positive)
         outputs3 = model(negative)
         loss = criterion(outputs1, outputs2, outputs3)
@@ -203,7 +209,7 @@ original_model = models.alexnet(pretrained=True)
 
 class AlexNetNoClassifier(nn.Module):
             def __init__(self):
-                super(AlexNetConv4, self).__init__()
+                super(AlexNetNoClassifier, self).__init__()
                 self.features = nn.Sequential(
                     *list(original_model.features.children())[:]
                 )
@@ -220,20 +226,20 @@ if args.cuda:
 def main():
 
     triplet_dataset, triplet_label, class_num = _get_triplet_data('train')
-    print('train data size', triplet_dataset.size())
+    print('train data  size: ', triplet_dataset.size())
     print('train target size', triplet_label.size())
     train_data = data_utils.TensorDataset(triplet_dataset, triplet_label)
     train_loader = data_utils.DataLoader(train_data, batch_size=args.train_batch_size, shuffle=True)
 
     cudnn.benchmark = True
 
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    optimizer = optim.SGD(new_model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
     criterion = nn.TripletMarginLoss(margin=1.0, p=2)
     if args.cuda:
         criterion = nn.TripletMarginLoss(margin=1.0, p=2).cuda()
 
     title = 'CUHK03-AlexNet'
-    date_time = get_datetime()
+   
     # Train
     for epoch in range(1, args.epochs + 1):
         lr, optimizer = exp_lr_scheduler(optimizer, epoch)
@@ -242,11 +248,11 @@ def main():
         train_model(train_loader, new_model, criterion, optimizer, epoch)
 
     # Test
-    model.eval()
+    new_model.eval()
 
-    torch.save(model, 'triplet_alexnet_trained.pth')
+    torch.save(new_model, 'triplet_alexnet_trained.pth')
 
-    score_array = cmc(model)
+    score_array = cmc(new_model)
     print(score_array)
 
     print('Top1(accuracy) : {:.3f}\t''Top5(accuracy) : {:.3f}'.format(
